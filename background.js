ac38e40318c01cfbb10d241bbcb468e700648f77
d3mol <markvaljak@gmail.com>
2017-04-24 14:36:24 +0300

    fix bug - pika user inputi bug
    Nüüd saab pikkasi stringe inputida nii, et ei teki probleeme

--- background.js
+++ background.js
@@ -75,31 +75,36 @@ var ref = database.ref("note");
 // submit data onkeypress
 window.addEventListener('keypress', function (e) {
     //kas keypress oli "Enter" ja et kas õige input field on active
-	if (e.keyCode == 13 && document.activeElement.id == "listInput") {
-		   var userNote = document.getElementById("listInput").value;
-		//input ei tohi tühi olla
-		if(userNote.length == 0){
-			alert("Tühi väli!");
-		} else {
-		var data = {
-		note: userNote
-		}
-		//andmebaasi saatmine
-		ref.push(data);
-		console.log(userNote);
-		}
-		//teen inputi tühjaks peale väärtuse sisestamist
-		document.getElementById("listInput").value = "";
-		
-		
-		
-		
-		
-    }
+	
+	if(document.getElementById("listInput").value.length > 50) {
+		alert("Ületasid maksimaalse sümbolite arvu 50!");
+	} else {
+	
+		if (e.keyCode == 13 && document.activeElement.id == "listInput") {
+			
+				
+			var userNote = document.getElementById("listInput").value;
+			//input ei tohi tühi olla
+			if(userNote.length == 0){
+				alert("Tühi väli!");
+			} else {
+			var data = {
+			note: userNote
+			}
+			//andmebaasi saatmine
+			ref.push(data);
+			console.log(userNote);
+			}
+			//teen inputi tühjaks peale väärtuse sisestamist
+			document.getElementById("listInput").value = "";
+			}
+		}	
+    
 }, false);
 
 // submit data onclick
 document.getElementById("submitButton").addEventListener("click", function(){
+	
 	var userNote = document.getElementById("listInput").value;
 	//input ei tohi tühi olla
 	if(userNote.length == 0){
@@ -114,6 +119,7 @@ document.getElementById("submitButton").addEventListener("click", function(){
 		//teen inputi tühjaks peale väärtuse sisestamist
 		document.getElementById("listInput").value = "";
 		}
+	
 });
 
 //remove elemendid class-i järgi
@@ -128,8 +134,8 @@ function removeElementsByClass(className){
 
 document.addEventListener("click", function(e) {
 	var click = e.target;
-	console.log("sinu click ID:  " + click.id);
-	console.log("sinu click CLASS:  " + click.className);
+	//console.log("sinu click ID:  " + click.id);
+	//console.log("sinu click CLASS:  " + click.className);
 	
 	//console.log(click);
 	// et hiirega mujale, kui listi clickides paneks listi kinni.
@@ -185,9 +191,42 @@ function gotData(data) {
 		var k = keys[i];
 		//console.log(k);
 		var oneNote = notes[k];
-		//console.log(notes[k]);
-		document.getElementById("dataDiv").innerHTML += '<ul class="'+ k +'"><li class="listItem">' + '<img id="'+k+'" class="deleteValue"src="https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-part-2/512/erase_delete_remove_wipe_out-512.png">'  + oneNote.note + '</li></ul>';
-		}
+		//console.log(oneNote.note);
+		
+			var getInnerText = oneNote.note;
+			//console.log(getInnerText);
+			
+			var words = getInnerText.split(' ');
+			
+			var startSentence = "";
+			var endSentence = "";
+			var e = 0;
+			//vaatab nt kui on suurem kui 5
+			if(getInnerText.length > 30){
+				
+				// tekitan esimene poole lausest
+				while(startSentence.length < 30){
+					startSentence += words[e++] + " ";
+				}
+				console.log("see on alguslause: " + startSentence);
+				document.getElementById("dataDiv").innerHTML += '<ul class="'+ k +'"><li class="listItem">' + '<img id="'+k+'" class="deleteValue"src="https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-part-2/512/erase_delete_remove_wipe_out-512.png"><span class="spanText">' + startSentence + '</span></li></ul>';
+				
+				// tekitan teise poole lausest 
+				while(e < words.length){
+					endSentence += words[e++] + " ";	
+				}
+				console.log("see on lõpulause: " + endSentence);
+				document.getElementById("dataDiv").innerHTML += '<ul class="'+ k +'"><li class="listItem"><span class="spanText">' + endSentence + '</span></li></ul>';
+				
+				
+			} else {
+				document.getElementById("dataDiv").innerHTML += '<ul class="'+ k +'"><li class="listItem">' + '<img id="'+k+'" class="deleteValue"src="https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-part-2/512/erase_delete_remove_wipe_out-512.png"><span class="spanText">' + oneNote.note + '</span></li></ul>';
+			}
+		
+		
+		
+	}
+	
 }
 
 function errData(err) {
@@ -241,106 +280,5 @@ function dragMoveListener (event) {
   }
 window.dragMoveListener = dragMoveListener;
 //-----------------Ikooni liigutamine lõppeb------------------
-
-
-
-
-
-//var titleElement = document.getElementById("dataDiv");
-//var titleChildren = titleElement.getElementsByTagName("img");
-
-//articles = titleElement.getElementsByTagName("img");
-
-//console.log(articles.length);
-
-//document.addEventListener('click', function(e) {
-//    alert(e.target.id);
-//});
-
-
-
-
-
-//-----------------Firebase algus-----------------
-/*
-var config = {
-    apiKey: "AIzaSyAo_hm_wJNKnUxt-U6xbjAT_TdpMLEPj7E",
-    authDomain: "note-bebd7.firebaseapp.com",
-    databaseURL: "https://note-bebd7.firebaseio.com",
-    projectId: "note-bebd7",
-    storageBucket: "",
-    messagingSenderId: "469109871476"
-};
-firebase.initializeApp(config);
-
-console.log(firebase);
-
-//connection to db
-var database = firebase.database();
-
-var ref = database.ref("note");
-
-
-
-
-
-//funktsioon, millega saadan user inputi firebase´i 
-function getNote(){
-	var userNote = document.getElementById("listInput").value;
-	console.log(userNote);
-	var data = {
-	note: userNote
-	}
-	ref.push(data);
-}
-
-ref.on("value", gotData, errData);
-
-//remove ul-id
-function removeElementsByClass(className){
-    var elements = document.getElementsByClassName(className);
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
-    }
-}
-
-function gotData(data){
-	//console.log(data.val());
-	var notes = data.val();
-	var keys = Object.keys(notes);
-	console.log(keys);
-	
-	// removeElemtnsByClass removib ära, et peale igat uut väärtust ei kirjutataks tervet tabelit topelt üle
-	removeElementsByClass("ul");
-	for( var i = 0; i < keys.length; i++){
-		var k = keys[i];
-		var oneNote = notes[k].note;
-		console.log(oneNote);
-		//list.innerHTML -= oneNote;
-		//document.getElementsByClassName("ul").value = "";
-
-		//list.innerHTML += "<ul class='ul'id='ul" + i + "'><li>" + oneNote +"</li><img src='https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-part-2/512/erase_delete_remove_wipe_out-512.png' id='" + i + "' width='15' height='15'></ul>";
-	}
-}
-
-function errData(err) {
-	console.log("Error!");
-	console.log(err);
-}
-*/
-
-
-/*
-function remove(key){
-	database.ref("note/"+key).remove()
-	.then(function(){
-		console.log("removed successfully");
-	})
-	.catch(function(error){
-		console.log(error);
-	});
-}
-*/
-
 
 
